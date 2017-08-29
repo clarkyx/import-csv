d8c1adf2fff9a66c84b2357db4309e6da39c4506
clark chen <chenhuajie1@gmail.com>
2017-08-29 13:45:10 -0400

    add debug mode

--- lib/split.rb
+++ lib/split.rb
@@ -10,56 +10,18 @@ class Split
     @temp_file_path = '../chunk.csv'
     @db = DatabaseConnection.connect
     @table = options[:table]
+    @debug = options[:debug]
     File.open(File.expand_path("config/rules.yml")) do |f|
        @ruleset = YAML.load(f)
        @rules = @ruleset.map {|k, v| self.send(v.to_sym, k.to_s)}.compact.join(", ")
     end
   end
 
-  def split
-    chunk_count = 0
-    # create a temp table to hold data
-    @db.query("CREATE TEMPORARY TABLE temp_%s LIKE %s" % [@table, @table])
-
-    chunks = size
-
-    File.open(@filepath) do |f|
-      f = Zlib::GzipReader.new(f) if not @filepath.include? 'csv'
-      f.each_line.each_slice(@chunksize).with_index(1) do |rows, i|
-        puts "Inserting chunk %s/%s" % [i, chunks]
-        File.open(@temp_file_path, 'w') do |chunk|
-          @db.query("TRUNCATE TABLE temp_%s" % @table)
-          chunk.write(rows.join)
-          chunk.close
-          insert
-        end
-      end
+  def debug
+    if (@debug)
+      yield
     end
-
-    puts 'Finish'
   end
-
-  def insert
-    columns = @ruleset.keys.join(', ')
-    @db.query(
-      "LOAD DATA LOCAL INFILE '%s'
-       INTO TABLE temp_%s
-       FIELDS TERMINATED BY ','
-       ENCLOSED BY '\"'
-       LINES TERMINATED BY '\n'" % [@temp_file_path, @table]
-    )
-    @db.query(
-      "INSERT INTO %{tn} (%{c})
-       SELECT %{c}
-       FROM temp_%{tn}
-       ON DUPLICATE KEY UPDATE %{r}" % {
-        tn: @table,
-        r: @rules,
-        c: columns
-      }
-    )
-  end
-
 
   def size
     size = File.open(@filepath).readlines.size
@@ -85,6 +47,66 @@ class Split
 
   def ignore(attribute)
     nil
+  end
+
+  def insert
+    columns = @ruleset.keys.join(', ')
+    query =
+      "LOAD DATA LOCAL INFILE '%s'
+       INTO TABLE temp_%s
+       FIELDS TERMINATED BY ','
+       ENCLOSED BY '\"'
+       LINES TERMINATED BY '\n'" % [@temp_file_path, @table]
+
+    debug do
+      puts "SQL > %s" % query
+    end
+
+    @db.query(query)
+
+
+    query =
+      "INSERT INTO %{tn} (%{c})
+       SELECT %{c}
+       FROM temp_%{tn}
+       ON DUPLICATE KEY UPDATE %{r}" % {
+        tn: @table,
+        r: @rules,
+        c: columns
+      }
+
+    debug do
+      puts "SQL > %s" % query
+    end
+
+    @db.query(query)
+
+  end
+
+  def split
+    chunk_count = 0
+    # create a temp table to hold data
+    @db.query("CREATE TEMPORARY TABLE temp_%s LIKE %s" % [@table, @table])
+
+    chunks = size
+
+    File.open(@filepath) do |f|
+      f = Zlib::GzipReader.new(f) if not @filepath.include? 'csv'
+      f.each_line.each_slice(@chunksize).with_index(1) do |rows, i|
+        puts "Inserting chunk %s/%s" % [i, chunks]
+        debug do
+          puts "chunk %d > %s" % [i, rows]
+        end
+        File.open(@temp_file_path, 'w') do |chunk|
+          @db.query("TRUNCATE TABLE temp_%s" % @table)
+          chunk.write(rows.join)
+          chunk.close
+          insert
+        end
+      end
+    end
+
+    puts 'Finish'
   end
 end
 
